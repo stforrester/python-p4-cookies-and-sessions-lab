a872721246f5de15007e85052b3c7c429653c2c2
Stuart Forrester <stforrester@gmail.com>
2025-02-05 14:11:43 -0600

    completes the lab and passes all tests, also updates the structure of the '/clear' route in app.py (api-side) and adds a 'Clear Session ' button into the Header.js component so clearing the session actually functions on the front-end too.

--- server/app.py
+++ server/app.py
@@ -17,18 +17,43 @@ db.init_app(app)
 
 @app.route('/clear')
 def clear_session():
-    session['page_views'] = 0
+    session.clear()
     return {'message': '200: Successfully cleared session data.'}, 200
 
 @app.route('/articles')
 def index_articles():
 
-    pass
+    session['page_views'] = session.get('page_views', 0)
+
+    articles = [article.to_dict() for article in Article.query.all()]
+
+    response = make_response(
+        articles,
+        200
+    )
+
+    return response
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    
+    session['page_views'] = session.get('page_views', 0) + 1
 
-    pass
+    if session['page_views'] > 3:
+        response = make_response(
+            {"message": "Maximum pageview limit reached"},
+            401
+        )
+        return response
+    
+    article = Article.query.filter_by(id=id).first().to_dict()
+
+    response = make_response(
+        article,
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
